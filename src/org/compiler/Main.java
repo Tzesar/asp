ed3106ebfcd6a701ff7c27d3f2e0d36ed6df5b01
Augusto Amarilla <agu.amarilla@gmail.com>
2015-10-29 16:00:55 -0300

    The files reader now supports comments.

--- src/org/compiler/Main.java
+++ src/org/compiler/Main.java
@@ -5,8 +5,13 @@ import org.compiler.Exceptions.MalformedProductionException;
 import org.compiler.Exceptions.SyntaxAnalyzerException;
 import org.compiler.Util.Constants;
 
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.Reader;
+import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
@@ -15,26 +20,6 @@ import java.util.stream.Collectors;
 public class Main {
 
     public static void main(String[] args) {
-//        String grammar = "E\t-> T H\n;" +
-//                "H\t-> a T H | @\n;" +
-//                "T\t-> F G\n;" +
-//                "G\t-> b F G | @\n;" +
-//                "F\t-> d E d | i;";
-
-//        String grammar = "T->a|@;E->Tc|b;";
-
-//        String grammar = "S->iEtSK | a; K -> eS | @; E ->b;";
-
-//        String grammar = "S->Sa;R->d | e;";
-
-//        String grammar = "E\t-> T K;\n" +
-//                "K\t-> + T K | @;\n" +
-//                "T\t-> F J;\n" +
-//                "J\t-> * F J | @;\n" +
-//                "F\t-> ( E ) | i;";
-//
-//        String input = "i+i";
-
         String grammar = getFileContent(Configurations.grammarPath);
         String input = getFileContent(Configurations.inputPath);
 
@@ -60,11 +45,13 @@ public class Main {
     private static String getFileContent(String path) {
         List<String> content = new ArrayList<>();
 
-        Path grammarFilePath = Paths.get(path);
+        Path filePath = Paths.get(path);
         try {
-            Scanner scanner = new Scanner(grammarFilePath, StandardCharsets.UTF_8.name());
-
-            scanner.forEachRemaining(content::add);
+            Files.lines(filePath, Charset.defaultCharset()).forEach( line -> {
+                if (!line.startsWith("//")) {
+                    content.add(line);
+                }
+            });
         } catch (IOException e){
             System.out.println("Working directory: "+ System.getProperty("user.dir") );
         }
